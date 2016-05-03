50ba08641c0b25b5af7d7704fec455b831f288d2
Doğan Derya <doganderya59@gmail.com>
2016-05-03 05:35:36 +0300

    logging for sequelize

--- db.js
+++ db.js
@@ -7,7 +7,8 @@ const config = require('config');
 logger.log('Connecting to database...');
 const db = new Sequelize(config.get('database.database'), config.get('database.user'), config.get('database.password'), {
   host: config.get('database.host'),
-  dialect: config.get('database.dialect')
+  dialect: config.get('database.dialect'),
+  logging: logger.level() > 0 ? null : logger.debug.bind(logger)
 });
 
 module.exports = db;
