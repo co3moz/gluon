7862181d47b34e0b023ece496ace987db67d1e48
Doğan Derya <doganderya59@gmail.com>
2016-06-06 10:28:25 +0300

    more database options

--- db.js
+++ db.js
@@ -5,10 +5,8 @@ const Sequelize = require('sequelize');
 const config = require('config');
 
 logger.log('Connecting to database...');
-const db = new Sequelize(config.get('database.database'), config.get('database.user'), config.get('database.password'), {
-  host: config.get('database.host'),
-  dialect: config.get('database.dialect'),
+const db = new Sequelize(config.get('database.database'), config.get('database.user'), config.get('database.password'), Object.assign({
   logging: logger.level() > 0 ? null : logger.debug.bind(logger)
-});
+}, config.get('database')));
 
 module.exports = db;
