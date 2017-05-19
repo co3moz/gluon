0293abb682ca86228ff835298bc95edc776ab49e
Doğan Derya <doganderya59@gmail.com>
2017-05-20 00:48:51 +0300

    * fixed recursive search.

--- utils/recursive-search.js
+++ utils/recursive-search.js
@@ -2,6 +2,10 @@ module.exports = function RecursiveSearch(obj, models) {
     Object.keys(obj).forEach(function (key) {
         var value = obj[key];
 
+        if (value == null) {
+            return;
+        }
+
         if (value.constructor == String) {
             if (value.indexOf("@@@") != 0) {
                 return;
